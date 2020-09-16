7a73bca63f351a5e89a35cd0ae8a1d5d5097e082
Otsuki Hitoshi <hitochan777@gmail.com>
2020-09-16 23:53:39 +0900

    WIP: combine sniffer and uploader

--- src/main.py
+++ src/main.py
@@ -1,31 +1,26 @@
-from datetime import datetime
-import logging
-import time
 import argparse
+import os
+import time
 
 from device_sniffer import DeviceSniffer
+from attendance_upload_service import AttendanceUploadService
 
 
-def print_capture(capture):
-    logging.info(f"{capture[0]} {capture[1]}")
-
-
-def main():
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--interface', '-i', default='mon0', help='monitor mode enabled interface')
-    parser.add_argument('--log-file', '-l', default='wifiscanner.log', help='path to filename to output log')
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',filename='wifiscanner.log',level=logging.DEBUG)
-    logging.info('\n' + '\033[93m' + 'Wifi Scanner Initialized' + '\033[0m' + '\n')
-    print('\n' + '\033[93m' + 'Wifi Scanner Initialized' + '\033[0m' + '\n')
-
+    connection_string = os.environ.get("IOTHUB_DEVICE_CONNECTION_STRING")
+    upload_service = AttendanceUploadService(connection_string)
     device_sniffer = DeviceSniffer(args.interface)
-    device_sniffer.start()
-    device_sniffer.get_observable().subscribe(print_capture)
-    input("Type any key to stop monitoring")
-    device_sniffer.stop()
+    try:
+        device_sniffer.start()
+        observable = device_sniffer.get_observable()
+        # observable.subscribe()
+        while True:
+            time.sleep(1)
 
-if __name__ == '__main__':
-    main()
+    except KeyboardInterrupt:
+        device_sniffer.stop()
+        print("Exiting program...")
