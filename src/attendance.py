01f1d6bc7d73d5851c87ccec8d64802b6470277b
Otsuki Hitoshi <hitochan777@gmail.com>
2020-09-17 23:45:22 +0900

    Add userid field to Attendance

--- src/attendance.py
+++ src/attendance.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from attendance_type import AttendanceType
 
 class Attendance:
-    def __init__(self, attendance_type: AttendanceType, occurredAt: datetime):
+    def __init__(self, userId: str, attendance_type: AttendanceType, occurredAt: datetime):
+        self.userId = userId
         self.attendance_type = attendance_type 
         self.occurredAt = occurredAt
