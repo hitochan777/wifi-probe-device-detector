86ef82eb82966b2620759f8c8a383f7dafbc3003
Otsuki Hitoshi <hitochan777@gmail.com>
2020-09-22 19:41:11 +0900

    Fix typos in attendance entity

--- src/attendance.py
+++ src/attendance.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from attendance_type import AttendanceType
 
 class Attendance:
-    def __init__(self, userId: str, attendance_type: AttendanceType, occurredAt: datetime):
-        self.userId = userId
+    def __init__(self, userid: str, attendance_type: AttendanceType, occurred_at: datetime):
+        self.userid = userid
         self.attendance_type = attendance_type 
-        self.occurredAt = occurredAt
+        self.occurred_at = occurred_at
