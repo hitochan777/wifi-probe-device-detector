255c4d9fa4a44a11e40cc566f56cf0c72a2e1432
Otsuki Hitoshi <hitochan777@gmail.com>
2020-10-25 19:38:23 +0900

    Add enable monitor mode on wakeup option

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import time
 import json
 from typing import List
 from dataclasses import dataclass
+import scapy
+import subprocess
+
 
 from src.device_sniffer import DeviceSniffer
 from src.attendance_upload_service import AttendanceUploadService
@@ -17,6 +20,7 @@ from src.file_queue import FileQueue
 class CommandLineArgs:
     interface: str
     queue_path: str
+    enable_monitor_wakeup: bool
     configs: List[Config]
 
 
@@ -25,6 +29,7 @@ def cmdline() -> CommandLineArgs:
     parser.add_argument('--interface', '-i', default='mon0', help='monitor mode enabled interface')
     parser.add_argument('--config', '-c', required=True, help='path to JSON config file')
     parser.add_argument('--queue-path', '-q', required=True, help='path for saving queue')
+    parser.add_argument('--enable-monitor-on-wakeup', '-w', action='store_true', help='Enable monitor mode on specified interface on startup')
     args = parser.parse_args()
 
     configs: List[Config] = []
@@ -33,11 +38,31 @@ def cmdline() -> CommandLineArgs:
         for raw_config in raw_configs:
             configs.append(Config(raw_config["userid"], raw_config["ssid"], raw_config["mac_address"], raw_config["absence_due_second"]))
 
-    return CommandLineArgs(interface=args.interface, queue_path=args.queue_path, configs=configs)
+    return CommandLineArgs(interface=args.interface, queue_path=args.queue_path, enable_monitor_wakeup=args.enable_monitor_on_wakeup, configs=configs)
 
+
+def enable_monitor_mode(interface: str) -> None:
+    cmd_result = subprocess.run(["ip", "link", "set", interface, "down"])
+    if cmd_result.returncode == -1:
+        raise RuntimeError()
+    
+    cmd_result = subprocess.run(["iw", interface, "set", "monitor", "none"])
+    if cmd_result.returncode == -1:
+        raise RuntimeError()
+
+    cmd_result = subprocess.run(["ip", "link", "set", interface, "up"])
+    if cmd_result.returncode == -1:
+        raise RuntimeError()
 
 if __name__ == "__main__":
     args = cmdline()
+
+    if args.enable_monitor_wakeup:
+        try:
+            enable_monitor_mode(args.interface)
+        except:
+            raise RuntimeError("Failed while enabling monitor mode")
+
     print(f"Listening on {args.interface}")
 
     queue = FileQueue(args.queue_path)
